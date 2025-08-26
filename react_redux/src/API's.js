15010c7389cf65e921c53152b3fc4531fff4a712
chkeethi2001 <chkeethi2001@gmail.com>
2025-08-26 11:36:07 +0530

    Update API's.js

--- react_redux/src/API's.js
+++ react_redux/src/API's.js
@@ -1,7 +1,7 @@
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
+    ? "http://localhost:5000/api"
     : "https://backend-deply-final-1.onrender.com/api";
 
-export const registerAPI = ${BASE_URL}/api/auth/register;
-export const loginAPI = ${BASE_URL}/api/auth/login;
+export const registerAPI = `${BASE_URL}/auth/register`;
+export const loginAPI = `${BASE_URL}/auth/login`;
